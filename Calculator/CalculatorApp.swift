eaaa91aa0dab03a88b10334cb71d7ca38cff90cb
arivanjuniordev <94536010+arivanjuniordev@users.noreply.github.com>
2024-08-14 13:30:59 -0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Arivan Júnior on 14/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
