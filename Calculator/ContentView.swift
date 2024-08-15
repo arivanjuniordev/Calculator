6c38c8cae005a743f650887039cb0632347d0a5b
arivanjuniordev <94536010+arivanjuniordev@users.noreply.github.com>
2024-08-15 11:28:11 -0300

    Update ContentView.swift

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -8,14 +8,236 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State var values = "0"
+    @State var previous = 0.0
+    @State var result = 0.0
+    @State var decimal = 0.0
+    
+    @State var operation = 0
+    @State var previousOperation = 0
+    
+    func removeZerosFromEnd(value: Double) -> String {
+        let f = NumberFormatter()
+        let number = NSNumber(value: value)
+        f.minimumFractionDigits = 0
+        f.maximumFractionDigits = 16
+        return f.string(from: number) ?? ""
+    }
+    
+    func reset(){
+        previous = 0
+        result = 0
+        previous = 0
+        operation = 0
+        decimal = 0
+        values = "0"
+    }
+    
+    func process(digit: Int){
+        if operation > 0{
+            result = 0
+            previousOperation = operation
+            operation = -1
         }
-        .padding()
+        
+        if decimal > 0.0 {
+            result = result + Double(truncating: NSNumber(value: (Double(digit) / decimal)))
+            
+            decimal = decimal * 10
+            values = "\(values)\(digit)"
+        }else{
+            result = (result * 10) + Double(digit)
+            values = removeZerosFromEnd(value: result)
+        }
+        
+    }
+    
+    func calculate(){
+        if previousOperation == 1 {
+            result = previous + result
+            previousOperation = 0
+        }else if previousOperation == 2{
+            result = previous - result
+            previousOperation = 0
+
+        }
+        else if previousOperation == 3{
+            result = previous * result
+            previousOperation = 0
+
+        }
+        else if previousOperation == 4{
+            result = previous / result
+            previousOperation = 0
+
+        }
+        
+        decimal = 0.0
+        previous = result
+        values = removeZerosFromEnd(value: result)
+        
+        previous = result
+    }
+   
+    var body: some View {
+        VStack(alignment: .trailing,spacing:0) {
+            Spacer()
+            
+            HStack{
+                Text(values)
+                .padding()
+                    .lineLimit(1)
+                    .font(.system(size: CGFloat(80 / Int(Double(String( result).count + 10)/10.0))))
+                    .foregroundColor(Color.white)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    .fixedSize(horizontal: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/, vertical: false)
+            }
+              
+        
+            HStack(spacing: 0){
+                Button("AC"){
+                    reset()
+                }
+                .padding()
+                .frame(maxWidth:/*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                Button("+/-"){
+                    result = result * -1
+                    calculate()
+                }
+                .padding()
+                .frame(maxWidth:/*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                Button("%"){
+                    result = result / 100
+                    calculate()
+                }
+                .padding()
+                .frame(maxWidth:/*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                Button("/"){
+                    calculate()
+                    operation = 4
+                }
+                .padding(.vertical, 40)
+                .frame(maxWidth:/*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .background(Color.orange)
+                
+            }.foregroundColor(Color.white)
+            
+            HStack(spacing: 0){
+                Button("7"){
+                    process(digit: 7)
+                }
+                .padding()
+                .frame(maxWidth:/*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                Button("8"){
+                    process(digit: 8)
+                }
+                .padding()
+                .frame(maxWidth:/*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                Button("9"){
+                    process(digit: 9)
+                }
+                .padding()
+                .frame(maxWidth:/*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                Button("X"){
+                    calculate()
+                    operation = 3
+                }
+                .padding(.vertical, 40)
+                .frame(maxWidth:/*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .background(Color.orange)
+                
+            }.foregroundColor(Color.white)
+            
+            HStack(spacing: 0){
+                Button("4"){
+                    process(digit: 4)
+                }
+                .padding()
+                .frame(maxWidth:/*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                Button("5"){
+                    process(digit: 5)
+                }
+                .padding()
+                .frame(maxWidth:/*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                Button("6"){
+                    process(digit: 6)
+                }
+                .padding()
+                .frame(maxWidth:/*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                Button("-"){
+                    calculate()
+                    operation = 2
+                }
+                .padding(.vertical, 40)
+                .frame(maxWidth:/*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .background(Color.orange)
+                
+            }.foregroundColor(Color.white)
+            
+            HStack(spacing: 0){
+                Button("1"){
+                    process(digit: 1)
+               
+
+                }
+                .padding()
+                .frame(maxWidth:/*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                Button("2"){
+                    process(digit: 2)
+
+                }
+                .padding()
+                .frame(maxWidth:/*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                Button("3"){
+                    process(digit: 3)
+                }
+                .padding()
+                .frame(maxWidth:/*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                Button("+"){
+                    calculate()
+                    operation = 1
+                }
+                .padding(.vertical, 40)
+                .frame(maxWidth:/*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .background(Color.orange)
+                
+            }.foregroundColor(Color.white)
+            
+            GeometryReader{ geometry in
+                HStack(spacing: 0){
+                    Button("0"){
+                        process(digit: 0)
+                    }
+                        .padding()
+                        .frame(minWidth: geometry.size.width / 2)
+                    
+                    Button("."){
+                        if decimal == 0.0{
+                            decimal = 10.0
+                            values = values + "."
+                        }
+                   
+                    }
+                        .padding()
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    
+                    Button("="){
+                        calculate()
+                        previousOperation = 999
+                        operation = 999
+                    }
+                        .padding(.vertical,40)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.orange)
+                    
+                }.foregroundColor(Color.white)
+            }.frame(maxHeight: 92)
+            
+        }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
+                maxHeight: .infinity)
+            .background(Color.black)
+        
+      
     }
 }
 
